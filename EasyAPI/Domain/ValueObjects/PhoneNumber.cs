9859d567f094adab46138d4960de59198610dfb1
Franco <francodugo.vidal@gmail.com>
2024-08-22 15:23:54 -0300

    ValueObjects: PhoneNumber VO created

---
+++ EasyAPI/Domain/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.ValueObjects;
+
+public partial record PhoneNumber
+{
+    private const int DefaultLenght = 10;
+    private const string Pattern = @"/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/";
+
+    private PhoneNumber(string value) => Value = value;
+
+    public static PhoneNumber? Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || !PhoneNumberRegex().IsMatch(value) || value.Length != DefaultLenght)
+            return null;
+
+        return new PhoneNumber(value);
+    }
+
+    public string Value { get; init; }
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex PhoneNumberRegex();
+}
