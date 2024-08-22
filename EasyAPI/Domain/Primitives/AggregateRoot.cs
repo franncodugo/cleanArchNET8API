792fda9f21add17edc582cc3566e15dcca407ee0
Franco <francodugo.vidal@gmail.com>
2024-08-22 15:01:16 -0300

    DomainLayer definition: AggregateRoot, DomainEvents, IUnitOfWork

---
+++ EasyAPI/Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Primitives;
+
+public abstract class AggregateRoot
+{
+    // State of Domain Events
+    private readonly List<DomainEvents> _domainEvents = new();
+
+    public ICollection<DomainEvents> GetDomainEvents() => _domainEvents;
+
+    protected void Raise(DomainEvents domainEvents)
+    {
+        _domainEvents.Add(domainEvents);
+    }
+}
---
+++ EasyAPI/Domain/Primitives/DomainEvents.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Domain.Primitives;
+
+public record DomainEvents(Guid Id) : INotification;
